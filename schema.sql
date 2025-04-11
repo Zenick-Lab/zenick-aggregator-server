10e1793e0f53abf27873836b9edaddd0fc8c42bb
akagiyuu <khanghuynh0245@gmail.com>
2025-04-11 22:41:10 +0700

    init

---
+++ schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS providers(
+    id serial PRIMARY KEY,
+    name text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS tokens(
+    id serial PRIMARY KEY,
+    name text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS operations(
+    id serial PRIMARY KEY,
+    name text NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS histories(
+    id serial PRIMARY KEY,
+
+    provider_id serial NOT NULL,
+    token_id serial NOT NULL,
+
+    operation_id serial NOT NULL,
+    apr real NOT NULL,
+
+    created_at timestamp default (timezone('utc', now())) NOT NULL,
+
+    FOREIGN KEY(provider_id) REFERENCES providers(id),
+    FOREIGN KEY(token_id) REFERENCES tokens(id),
+    FOREIGN KEY(operation_id) REFERENCES operations(id)
+);
