c67592d058cbb21bfdb57c725c98e36a50f8fc9d
akagiyuu <khanghuynh0245@gmail.com>
2025-04-11 22:59:06 +0700

    feat: add init data

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO providers(name) VALuES
+    ('suilend'),
+    ('naviprotocol');
+
+INSERT INTO tokens(name) VALUES
+    ('Sui'),
+    ('USDC');
+
+INSERT INTO operations(name) VALUES
+    ('borrow'),
+    ('lend'),
+    ('stake');
