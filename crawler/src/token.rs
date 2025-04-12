03a461bf47bd369dca36847727ec794d571f2a38
akagiyuu <khanghuynh0245@gmail.com>
2025-04-12 23:42:17 +0700

    feat: function to convert token from String

--- crawler/src/token.rs
+++ crawler/src/token.rs
@@ -1,7 +1,23 @@
+use std::str::FromStr;
+
+use anyhow::bail;
+use serde::{Deserialize, Serialize};
 use strum::IntoStaticStr;
 
-#[derive(IntoStaticStr)]
+#[derive(IntoStaticStr, Serialize, Deserialize)]
 pub enum Token {
     Sui,
-    USDC,
+    Usdc,
+}
+
+impl FromStr for Token {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "sui" => Ok(Token::Sui),
+            "usdc" => Ok(Token::Usdc),
+            _ =>  bail!("invalid token name")
+        }
+    }
 }
