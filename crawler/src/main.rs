8f2a378ac6b03e5dd60004840d18a5b7f5fbe11a
akagiyuu <khanghuynh0245@gmail.com>
2025-04-15 00:52:36 +0700

    feat: use multiple chromium instance

--- crawler/src/main.rs
+++ crawler/src/main.rs
@@ -4,34 +4,32 @@ mod database;
 mod operation;
 mod provider;
 mod token;
+mod util;
 
 use std::sync::Arc;
 
 use anyhow::Result;
 use chromiumoxide::Browser;
 use database::History;
-use provider::navi::Navi;
+use provider::{navi::Navi, suilend::Suilend};
 use tokio::{sync::mpsc, task::JoinSet};
 
 async fn into_task(
-    fetch_func: impl AsyncFn(&Browser) -> Result<Vec<History>>,
-    browser: Arc<Browser>,
+    fetch_func: impl AsyncFn() -> Result<Vec<History>>,
     history_sender: mpsc::UnboundedSender<History>,
 ) {
-    let histories = fetch_func(&browser).await.unwrap();
+    let histories = fetch_func().await.unwrap();
     for history in histories {
         history_sender.send(history).unwrap();
     }
 }
 
-async fn spawn_tasks(browser: Arc<Browser>, history_sender: mpsc::UnboundedSender<History>) {
+async fn spawn_tasks(history_sender: mpsc::UnboundedSender<History>) {
     let mut join_set = JoinSet::new();
 
-    join_set.spawn(into_task(
-        Navi::fetch,
-        browser.clone(),
-        history_sender.clone(),
-    ));
+    join_set.spawn(into_task(Navi::fetch, history_sender.clone()));
+
+    join_set.spawn(into_task(Suilend::fetch, history_sender.clone()));
 
     join_set.join_all().await;
 }
@@ -48,9 +46,7 @@ async fn main() -> Result<()> {
         }
     });
 
-    let browser = Arc::new(browser::new().await);
-
-    spawn_tasks(browser, history_sender).await;
+    spawn_tasks(history_sender).await;
 
     history_task.await?;
 
