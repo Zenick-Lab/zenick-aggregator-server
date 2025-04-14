53a416acec92587192d68283159e8e55139d75d7
akagiyuu <khanghuynh0245@gmail.com>
2025-04-15 00:51:36 +0700

    feat: add suilend

---
+++ crawler/src/provider/suilend.rs
@@ -0,0 +1,66 @@
+use anyhow::Result;
+use chromiumoxide::Browser;
+use futures::{StreamExt, TryStreamExt, stream};
+use itertools::multizip;
+
+use crate::{browser, database::History, operation::Operation, token::Token, util};
+
+use super::Provider;
+
+const LINK: &str = "https://suilend.fi";
+
+const TOKEN_SELECTOR: &str = "tr.border-b > td:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > p:nth-child(1)";
+const LEND_APR_SELECTOR: &str = "tr.border-b > td:nth-child(5) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2)";
+const BORROW_APR_SELECTOR: &str = "tr.border-b > td:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2)";
+
+pub struct Suilend;
+
+impl Suilend {
+    pub async fn fetch() -> Result<Vec<History>> {
+        let browser = browser::new().await;
+
+        let page = browser::create_steath_page(&browser).await?;
+
+        page.goto(LINK).await?;
+        page.wait_for_navigation().await?;
+
+        let tokens = util::find_elements(TOKEN_SELECTOR, &page).await?;
+        let lend_aprs = util::find_elements(LEND_APR_SELECTOR, &page).await?;
+        let borrow_aprs = util::find_elements(BORROW_APR_SELECTOR, &page).await?;
+
+        let data = stream::iter(multizip((tokens, lend_aprs, borrow_aprs)))
+            .map(|(token, lend_apr, borrow_apr)| async move {
+                let token = token.inner_text().await.unwrap().unwrap();
+                let token = token.parse::<Token>().ok()?;
+
+                let raw = lend_apr.inner_text().await.unwrap().unwrap();
+                let lend_apr = util::parse_float(&raw).unwrap();
+
+                let raw = borrow_apr.inner_text().await.unwrap().unwrap();
+                let borrow_apr = util::parse_float(&raw).unwrap();
+
+                Some(stream::iter([
+                    History {
+                        provider: Provider::Navi,
+                        operation: Operation::Lend,
+                        token,
+                        apr: lend_apr,
+                    },
+                    History {
+                        provider: Provider::Navi,
+                        operation: Operation::Borrow,
+                        token,
+                        apr: borrow_apr,
+                    },
+                ]))
+            })
+            .buffer_unordered(20)
+            .filter_map(|x| async move { x })
+            .flatten()
+            .collect()
+            .await;
+
+        eprintln!("DEBUGPRINT[138]: suilend.rs:60: data={:#?}", data);
+        Ok(data)
+    }
+}
