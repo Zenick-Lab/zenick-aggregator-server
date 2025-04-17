22e0841c6cf099ecf3e205e724bb7bad30488e22
akagiyuu <khanghuynh0245@gmail.com>
2025-04-17 22:53:25 +0700

    feat: add extra chromium flags

--- crawler/src/browser.rs
+++ crawler/src/browser.rs
@@ -2,10 +2,30 @@ use anyhow::Result;
 use chromiumoxide::{Browser, BrowserConfig, Page};
 use futures::StreamExt;
 
+const BROWSER_ARGS: [&str; 16] = [
+    "--profile-directory=Default",
+    "--disable-gpu",
+    "--disable-extensions",
+    "--disable-dev-shm-usage",
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-infobars",
+    "--disable-notifications",
+    "--disable-popup-blocking",
+    "--disable-background-timer-throttling",
+    "--disable-backgrounding-occluded-windows",
+    "--disable-breakpad",
+    "--disable-component-extensions-with-background-pages",
+    "--disable-features=TranslateUI,BlinkGenPropertyTrees",
+    "--disable-ipc-flooding-protection",
+    "--disable-renderer-backgrounding",
+];
+
 pub async fn new() -> Browser {
     let (browser, mut handler) = Browser::launch(
         BrowserConfig::builder()
             .viewport(None)
+            .args(BROWSER_ARGS)
             .build()
             .unwrap(),
     )
